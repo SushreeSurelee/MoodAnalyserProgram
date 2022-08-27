6ffcd4e1a1da6fbb8da078e0f33e30e54111c957
“Sushree” <sushreesureleesahu98@gmail.com>
2022-08-27 23:42:45 +0530

    [Sushree | Initial commit]

---
+++ MoodAnalyserProgram/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyser Application\n");
+            Console.ReadLine();
+        }
+    }
+}
